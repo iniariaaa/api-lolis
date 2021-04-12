1e99ca4189c9befb95ea4e6f0508adb7fdc0776b
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-04-12 22:55:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+__path = process.cwd()
+
+var express = require('express');
+var router = express.Router();
+var { exec } = require('child_process')
+var fetch = require('node-fetch')
+var fs = require('fs')
+router.get('/ttp', (req, res) => {
+  if (!req.query.q) return res.json({ status: 404, error: 'masukkan param q'})
+data = await fetch(`https://api.areltiyan.site/sticker_maker?text=${encodeURIComponent(req.query.q)}`).then(v => v.json())
+         base64 = data.base64
+         var buffer = base64.slice(22)
+         await fs.writeFileSync('ttp.png', buffer, 'base64')
+    exec(`ffmpeg -i ${__path+'/ttp.png'} ${__path+'/ttp.webp'}`, (err) => {
+    res.sendFile(__path+'/ttp.webp')
+    })
+})
+const ram = `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*`
+port = process.env.PORT || 8080 || 5000 || 3000
+router.get('/', (req, res) => {
+    config = {
+        status: true,
+        info: {
+        memory: ram,
+        port: port
+        }
+    }
+    res.json(config)
+})
+
+module.exports = router
